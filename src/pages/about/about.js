cd16de02bc8e88c02d6b96fccd98786549333369
JLLoppez <99202989+JLLoppez@users.noreply.github.com>
2024-06-24 21:30:44 +0200

    latest commit

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -18,43 +18,88 @@ const About = React.forwardRef((props, ref) => {
                     height="200" 
                 />
                 <p className="about-us-button-text">
-                    Healing is more than just an online medical service; it’s a movement towards accessible, efficient, and compassionate healthcare for all. Founded by a team of visionary doctors, healthcare professionals, and technology experts, we are driven by the mission to deliver exceptional medical care directly to you, no matter where you are. Our platform is built on the pillars of trust, innovation, and patient-centricity, ensuring that every interaction is personalized and every treatment plan is tailored to your unique needs. With a network of licensed practitioners from diverse medical fields, we guarantee comprehensive care that’s just a click away.
+                Experience the future of healthcare with Healing, a digital platform designed to make quality medical care accessible and convenient for everyone. Our team of dedicated professionals is committed to delivering personalized, compassionate care from a diverse network of licensed practitioners. Join the movement towards better health – start your journey with Healing today.
+                <p>Healing leverages cutting-edge technology to enhance your healthcare experience. Our secure platform ensures seamless communication, convenient scheduling, and access to your medical records anytime, anywhere. Through virtual visits, digital prescriptions, and personalized health insights, we empower you to take charge of your health and make informed decisions.</p>
                 </p>
                 <button type="submit" className="about-us-button">
-                    <span className="book-top">Learn more about us</span>
+                    <span className="about-us-button-span">Learn more about us</span>
                 </button>
             </div>
-            {/* <div className="about-us-how">
-                <h1 className="about-us-how-h1">
-                     How <span>our platform</span> works
-                </h1>
-                <p className="about-us-how-para">
-                    Navigating your healthcare journey with Heading is seamless. Just follow these steps mentioned below to proceed with your selected services. You can also see our FAQ section for more guidance:                
-                </p>
-                <ol className="about-us-how-guide">
-                    <li className="about-us-guide-1">
+            <div className="about-us-how">
+                <div >
+                        <h1 className="about-us-how-h1">How
+                            <span> our platform</span> works
+                        </h1>
+                        <p className="about-us-how-para">
+                        Are you ready to take charge of your health and well-being? Healing is here to guide you through a seamless healthcare journey. Whether you're seeking preventative care, managing a chronic condition, or exploring specialized treatments, we've got you covered.
+                        </p>
+                </div>
+                <ol class="about-us-how-guide">
+                    <li  className="about-us-how-guide-1">
+                        <svg width="52" height="52" viewBox="0 0 52 52" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <rect width="52" height="52" rx="26" fill="#000080" fill-opacity="0.82"/>
+                            <path d="M25.7244 38V19.168L21.4044 21.808V17.552L25.7244 14.96H29.6604V38H25.7244Z" fill="white"/>
+                        </svg>
                         <span>Create Your Profile</span>
-                        <p>Sign up and fill in your medical history securely. Setting up your profile this way would ensure that you stay up-to-date with your medical processes.</p>
+                        <p>Sign up and complete your medical history confidentially to ensure personalized care and stay informed about your health progress. Your secure profile allows you to easily track appointments, access your medical records, and receive updates on your treatment plan.</p>
                     </li>
-                    <li className="about-us-guide-2">
-                        <span>Choose Your Service</span> 
-                        <p>Select from our range of services and book a consultation. Booking a consultation with HealNet is fairly simple and straight-forward.</p>
+                    <li className="about-us-how-guide-2">
+                        <svg width="52" height="52" viewBox="0 0 52 52" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <rect width="52" height="52" rx="26" fill="#000080" fill-opacity="0.82"/>
+                            <path d="M18.0688 37.968V34.608L27.9248 25.824C28.6928 25.1413 29.2261 24.496 29.5248 23.888C29.8234 23.28 29.9728 22.6933 29.9728 22.128C29.9728 21.36 29.8074 20.6773 29.4768 20.08C29.1461 19.472 28.6874 18.992 28.1008 18.64C27.5248 18.288 26.8581 18.112 26.1008 18.112C25.3008 18.112 24.5914 18.2987 23.9728 18.672C23.3648 19.0347 22.8901 19.52 22.5488 20.128C22.2074 20.736 22.0474 21.392 22.0688 22.096H18.1008C18.1008 20.56 18.4421 19.2213 19.1248 18.08C19.8181 16.9387 20.7674 16.0533 21.9728 15.424C23.1888 14.7947 24.5914 14.48 26.1808 14.48C27.6528 14.48 28.9754 14.8107 30.1488 15.472C31.3221 16.1227 32.2448 17.0293 32.9167 18.192C33.5888 19.344 33.9248 20.672 33.9248 22.176C33.9248 23.2747 33.7754 24.1973 33.4768 24.944C33.1781 25.6907 32.7301 26.3787 32.1328 27.008C31.5461 27.6373 30.8154 28.336 29.9408 29.104L22.9488 35.296L22.5968 34.336H33.9248V37.968H18.0688Z" fill="white"/>
+                        </svg>
+                        <span>Choose Your Service</span>
+                        <p>Ready to start your healing journey? Explore our range of services and book a consultation with ease. Our streamlined booking process makes it simple and straightforward to connect with the right specialist for your needs.</p>
                     </li>
-
-                    <li className="about-us-guide-3">
-                        <span> Meet Your Doctor</span>
-                        <p>Have a virtual consultation with one of our certified specialists, or go for a physical visit to the doctor in case you opted for a physical check-up.</p>
+                    <li className="about-us-how-guide-3 ">
+                        <svg width="52" height="52" viewBox="0 0 52 52" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <rect width="52" height="52" rx="26" fill="#000080" fill-opacity="0.82"/>
+                            <path d="M26.0351 38.432C24.8831 38.432 23.7951 38.208 22.7711 37.76C21.7471 37.312 20.8565 36.6773 20.0991 35.856C19.3525 35.0347 18.8138 34.064 18.4831 32.944L22.2111 31.936C22.4778 32.8533 22.9631 33.5627 23.6671 34.064C24.3818 34.5547 25.1658 34.7947 26.0191 34.784C26.7765 34.784 27.4538 34.608 28.0511 34.256C28.6485 33.904 29.1178 33.4293 29.4591 32.832C29.8005 32.224 29.9711 31.5467 29.9711 30.8C29.9711 29.6587 29.6031 28.704 28.8671 27.936C28.1311 27.168 27.1818 26.784 26.0191 26.784C25.6671 26.784 25.3205 26.832 24.9791 26.928C24.6485 27.024 24.3285 27.1573 24.0191 27.328L22.2591 24.288L30.0031 17.664L30.3391 18.592H19.3791V14.96H33.5071V18.608L27.2831 24.592L27.2511 23.456C28.6485 23.552 29.8378 23.9413 30.8191 24.624C31.8111 25.3067 32.5685 26.1867 33.0911 27.264C33.6245 28.3413 33.8911 29.52 33.8911 30.8C33.8911 32.2507 33.5338 33.552 32.8191 34.704C32.1151 35.856 31.1658 36.768 29.9711 37.44C28.7871 38.1013 27.4751 38.432 26.0351 38.432Z" fill="white"/>
+                        </svg>
+                        <span>Meet Your Doctor</span>
+                        <p>Not sure if you need a virtual or in-person doctor appointment? We offer both options to meet your healthcare needs. Certified specialists available for online consultations and physical exams.</p>
                     </li>
                 </ol>
-         
-                <p className="about-us-team">
-                    <svg className="about-us-team-svg" width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <rect width="28" height="28" rx="4" fill="#B6FFC6"/>
-                    <path d="M14 19.3995L18.3649 21.8549C19.1642 22.3049 20.1424 21.6397 19.932 20.7984L18.7751 16.1811L22.6351 13.0703C23.3398 12.5029 22.9611 11.4268 22.0356 11.3583L16.9555 10.9573L14.9676 6.59428C14.61 5.80191 13.39 5.80191 13.0324 6.59428L11.0445 10.9475L5.96443 11.3486C5.03887 11.417 4.66023 12.4931 5.36492 13.0605L9.22494 16.1713L8.06799 20.7886C7.85763 21.6299 8.83578 22.2951 9.63513 21.8451L14 19.3995Z" fill="#000080d3"/>
+
+                <div className="about-us-team">
+                <img 
+                    alt=""
+                    src={photo3} 
+                    width="1" 
+                    height="1"
+                    /> 
+                    <svg width="620" height="383" viewBox="0 0 363 329" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <g filter="url(#filter0_d_52_240)">
+                        <rect x="19" y="13" width="319" height="284" rx="24" fill="url(#paint0_linear_52_240)" shape-rendering="crispEdges"/>
+                        <rect x="20" y="14" width="317" height="282" rx="23" stroke="#000080" stroke-opacity="0.82" stroke-width="2" shape-rendering="crispEdges"/>
+                        </g>
+                        <defs>
+                        <filter id="filter0_d_52_240" x="0.268294" y="0.512197" width="362.707" height="327.707" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                        <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+                        <feOffset dx="3.12195" dy="9.36585"/>
+                        <feGaussianBlur stdDeviation="10.9268"/>
+                        <feComposite in2="hardAlpha" operator="out"/>
+                        <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.06 0"/>
+                        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_52_240"/>
+                        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_52_240" result="shape"/>
+                        </filter>
+                        <linearGradient id="paint0_linear_52_240" x1="163.344" y1="84" x2="163.343" y2="191.724" gradientUnits="userSpaceOnUse">
+                        <stop stop-color="#0F4D92" stop-opacity="0.24"/>
+                        <stop offset="0.436049" stop-color="#0047AB" stop-opacity="0.65"/>
+                        <stop offset="0.820643" stop-color="#000080" stop-opacity="0.79"/>
+                        </linearGradient>
+                        </defs>
                     </svg>
-                    <span>Best Certified Team of Specialists</span>
-                </p>
-            </div> */}
+                    <p className="about-us-team-p">
+                        <svg  width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <rect width="28" height="28" rx="4" fill="#CACAD7"/>
+                        <path d="M14 19.3995L18.3649 21.8549C19.1642 22.3049 20.1424 21.6397 19.932 20.7984L18.7751 16.1811L22.6351 13.0703C23.3398 12.5029 22.9611 11.4268 22.0356 11.3583L16.9555 10.9573L14.9676 6.59428C14.61 5.80191 13.39 5.80191 13.0324 6.59428L11.0445 10.9475L5.96443 11.3486C5.03887 11.417 4.66023 12.4931 5.36492 13.0605L9.22494 16.1713L8.06799 20.7886C7.85763 21.6299 8.83578 22.2951 9.63513 21.8451L14 19.3995Z" fill="#000080d3"/>
+                        </svg>
+                        <span>Best Certified Team of Specialists</span>
+                    </p>
+                </div>
+            </div>
         </div>
     );
 });
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,8 +1,96 @@
 import './home.scss';
-import photo1  from '../images/photo1.png';
-import React from "react";
+import photo1 from '../images/photo1.png';
+import React, { useEffect, useRef, useState } from "react";
+import PhoneInput from 'react-phone-input-2';
+import 'react-phone-input-2/lib/bootstrap.css'; 
 
-const Home = React.forwardRef ((props, ref)=> {
+
+
+// Calendly Object Reference
+
+
+// Home Component
+const Home = React.forwardRef((props, ref) => {
+  // Form Data State
+  const [formData, setFormData] = useState({
+    email: '',
+    contact: '',
+    countryCode: '', 
+    isValidContact: false,
+    isValidEmail: false
+  });
+
+  const handlePhoneChange = (value, data) => {
+    setFormData({
+      ...formData,
+      contact: value,
+      countryCode: data.dialCode,
+      isValidContact: true, // Adjust validation logic if needed
+    });
+  };
+
+  const [showErrors, setShowErrors] = useState(false);
+
+  const validateEmail = (email) => {
+    // Basic email validation regex (improve as needed)
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(String(email).toLowerCase());
+  };
+
+  const calendlyRef = useRef(window.Calendly);
+  // Calendly Script Loading
+    useEffect(() => {
+        setFormData(prevFormData => ({
+            ...prevFormData,
+            isValidEmail: validateEmail(prevFormData.email),
+          }));
+        }, [formData.email]); 
+        const script = document.createElement('script');
+        script.src = "https://assets.calendly.com/assets/external/widget.js";
+        script.async = true;
+        document.head.appendChild(script);
+    
+  // Input Change Handler
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  // Calendly Popup Function
+  const handleCalendlyClick = () => {
+    if (calendlyRef.current) {  
+      calendlyRef.current.initPopupWidget({ url: 'YOUR_CALENDLY_LINK' });
+    } else {
+      console.error("Calendly script not loaded yet.");
+    }
+    return false;
+  };
+
+  // Form Submission Handler
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setShowErrors(true); // Show errors if the validation fails
+
+    if (formData.isValidEmail && formData.isValidContact) {
+      handleCalendlyClick();
+        e.preventDefault();
+        handleCalendlyClick(); // Open Calendly popup
+        try {
+        const response = await fetch('/api/bookAppointment', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(formData)
+        });
+        if (response.ok) {
+            console.log('Booking successful');
+        } else {
+            console.error('Booking failed');
+        }
+        } catch (error) {
+        console.error('Error:', error);
+        }
+    }
+  };
+
     return (
         <div ref={ref}>
                <div className="wrapper">
@@ -15,12 +103,12 @@ const Home = React.forwardRef ((props, ref)=> {
                                     <span> trusted partner</span> in digital healthcare.
                                     </h1>
                                     <p className="Emp">
-                                        <span>Empowering Your Health, Every Step of the Way.</span> Experience personalized medical care from the comfort of your home. Connect with 
-                                        <span> certified doctors</span> or manage prescriptions, and schedule appointments with ease. Ready to take control of your health?
-                                        <span> Get Started</span> or Book an Appointment today.
+                                        <span>Your Health, Your Way: Personalized Virtual Medical Care </span> Experience convenient, tailored
+                                        <span> medical care</span> from the comfort of your home. 
+                                        <span> Consult with certified doctors online, manage medications, and Schedule appointments effortlessly.</span> Take charge of your well-being today.
                                     </p>
                                     <div className="booking">
-                                    <a className="btn" href="/#">Book an Appointment</a>
+                                    <a className="btn" href="/">Book an Appointment</a>
                                     </div>
                                 </div>
                                 <div className="docImage">
@@ -35,8 +123,8 @@ const Home = React.forwardRef ((props, ref)=> {
                                             <image href={photo1} width="1" height="1" /> 
                                             </pattern>
                                             <linearGradient id="paint0_linear_39_22" x1="250" y1="6.59504" x2="300" y2="476" gradientUnits="userSpaceOnUse">
-                                            <stop stopColor="#00F462"/>
-                                            <stop offset="0.678457" stopColor="#C2FFDB"/>
+                                            <stop stopColor="#3333eab4"/>
+                                            <stop offset="0.678457" stopColor="#3333eab4"/>
                                             </linearGradient>
                                         </defs>
 
@@ -45,47 +133,13 @@ const Home = React.forwardRef ((props, ref)=> {
                                         </svg>
                                 </div>
                             </div>
-                        {/* <div className="linear">
-                        <svg width="1440" height="594" viewBox="0 0 1440 594" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M1440 2.91192C1377.69 2.91192 1315.28 -5.31503 1260.06 27.652C1225.69 48.1733 1203.43 80.1683 1184.14 113.515C1159.8 155.578 1143.2 204.341 1105.03 235.76C1066.86 267.178 1019.65 286.156 972.923 296.3C916.298 308.593 862.634 272.529 824.418 236.051C777.451 191.219 744.42 130.362 743.636 65.7808C743.376 44.3243 760.453 20.5938 784.331 17.9015C825.582 13.2502 831.601 131.49 832.617 154.554C833.953 184.844 835.791 217.534 825.936 246.82C805.405 307.837 707.027 329.708 692.076 333.046C677.124 336.385 575.288 361.391 476.607 376.996C377.925 392.602 263.92 383.588 165.475 414.252C115.568 429.797 63.3955 458.754 31.0914 499.096C6.32301 530.027 -0.14602 554.534 -3.5 591.5" stroke="url(#paint0_linear_48_11)" strokeWidth="4" strokeLinecap="round" stroke-dasharray="12 12"/>
-                            <defs>
-                            <linearGradient id="paint0_linear_48_11" x1="3.43019" y1="499.473" x2="1376.94" y2="-36.2909" gradientUnits="userSpaceOnUse">
-                            <stop stopColor="#00F462" stop-opacity="0.42"/>
-                            <stop offset="0.563099" stopColor="#00F453" stop-opacity="0.11"/>
-                            <stop offset="0.887261" stopColor="#00F470" stop-opacity="0.6"/>
-                            </linearGradient>
-                            </defs>
-                        </svg>
-
-                        </div> */}
                         </div>
                     </div>
                 </div>
-                {/* <div className="trustees">
-                    <h3 className="trusted">Trusted by millions across the globe:</h3>
-                    <ul className="trustees-icons">
-                        <li className="amazon">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                                <path d="M257.2 162.7c-48.7 1.8-169.5 15.5-169.5 117.5 0 109.5 138.3 114 183.5 43.2 6.5 10.2 35.4 37.5 45.3 46.8l56.8-56S341 288.9 341 261.4V114.3C341 89 316.5 32 228.7 32 140.7 32 94 87 94 136.3l73.5 6.8c16.3-49.5 54.2-49.5 54.2-49.5 40.7-.1 35.5 29.8 35.5 69.1zm0 86.8c0 80-84.2 68-84.2 17.2 0-47.2 50.5-56.7 84.2-57.8v40.6zm136 163.5c-7.7 10-70 67-174.5 67S34.2 408.5 9.7 379c-6.8-7.7 1-11.3 5.5-8.3C88.5 415.2 203 488.5 387.7 401c7.5-3.7 13.3 2 5.5 12zm39.8 2.2c-6.5 15.8-16 26.8-21.2 31-5.5 4.5-9.5 2.7-6.5-3.8s19.3-46.5 12.7-55c-6.5-8.3-37-4.3-48-3.2-10.8 1-13 2-14-.3-2.3-5.7 21.7-15.5 37.5-17.5 15.7-1.8 41-.8 46 5.7 3.7 5.1 0 27.1-6.5 43.1z"/>
-                            </svg>
-                        </li>
-                        <li className="google">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512">
-                                <path d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"/>
-                            </svg>
-                        </li>
-                        <li className="apple">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
-                                <path d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z"/>
-                            </svg>
-                        </li>
-                    </ul>
-                </div> */}
-
 
                 <div className="top-header">
                     <h2 className="top-header-h2">Easily book an appointment in 3 simple steps.</h2>
-                    <form className="input-form">
+                    <form className="input-form" onSubmit={handleSubmit}>
                         <div className="input-group">
                             <div className="input-group-email">
                                 <label htmlFor="email" className="icon-label">
@@ -94,7 +148,16 @@ const Home = React.forwardRef ((props, ref)=> {
                                     </svg>
                                     <span className="email-add">Email Address</span>
                                 </label>
-                                <input type="email" id="email" placeholder="Enter Your Email Address" className="email-input email-placeholder" />
+                                <input 
+                                    type="email" 
+                                    id="email" 
+                                    name="email" // Add name attribute
+                                    placeholder="Enter Your Email Address" 
+                                    className="email-input email-placeholder" 
+                                    value={formData.email}
+                                    onChange={handleChange} // Attach the handler
+                                />
+                               {showErrors && !formData.isValidEmail && <p className="error-message">Invalid email address</p>}
                             </div>
 
                             <div className="input-group-contact">
@@ -104,8 +167,18 @@ const Home = React.forwardRef ((props, ref)=> {
                                     </svg>
                                     <span className="contact-add">Contact Number</span>
                                 </label>
-                                <input type="tel" id="contact" placeholder="Enter Your Contact Number" className="contact-input" />
-                            </div>
+                                <PhoneInput
+                                    country={'za'} 
+                                    value={formData.contact}
+                                    onChange={handlePhoneChange}
+                                    inputProps={{
+                                    name: 'contact',
+                                    required: true,
+                                    autoFocus: true
+                                    }}
+                                />
+                               {showErrors && !formData.isValidContact && <p className="error-message">Invalid phone number</p>}
+                                </div>
 
                             <div className="input-group-date">
                                 <label htmlFor="appointment" className="icon-label">
@@ -113,14 +186,20 @@ const Home = React.forwardRef ((props, ref)=> {
                                     <path fill='#000080d3' d="M400 64h-48V12c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v52H160V12c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v52H48C21.5 64 0 85.5 0 112v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48zm-6 400H54a6 6 0 0 1 -6-6V160h352v298a6 6 0 0 1 -6 6zm-52.8-200.7L198.8 404.5c-4.7 4.7-12.3 4.6-17-.1l-75.1-75.7c-4.7-4.7-4.6-12.3 .1-17l22.7-22.5c4.7-4.7 12.3-4.6 17 .1l44.1 44.5 111.1-110.2c4.7-4.7 12.3-4.6 17 .1l22.5 22.7c4.7 4.7 4.6 12.3-.1 17z"/></svg>
                                     <span className="date-top">Date of Appointment</span>
                                 </label>
-                                <input type="date" id="appointment" placeholder="Select Date of Appointment" className="date-input" />
+                                <input 
+                                    type="text"
+                                    id="appointment"
+                                    placeholder="Click to Schedule" 
+                                    readOnly 
+                                    onClick={handleCalendlyClick} 
+                                    className="date-input" 
+                                />
                             </div>
-
                             <div className="input-group-book">
                             <span className="email-add"></span>
                                 <button type="submit" className="book-button">
                                     <span className="book-top">Book Now</span>
-                                    <svg className='book-icon'  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                    <svg className='book-icon'  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill='#f3efef' >
                                         <path d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm0 48c110.5 0 200 89.5 200 200 0 110.5-89.5 200-200 200-110.5 0-200-89.5-200-200 0-110.5 89.5-200 200-200m140.2 130.3l-22.5-22.7c-4.7-4.7-12.3-4.7-17-.1L215.3 303.7l-59.8-60.3c-4.7-4.7-12.3-4.7-17-.1l-22.7 22.5c-4.7 4.7-4.7 12.3-.1 17l90.8 91.5c4.7 4.7 12.3 4.7 17 .1l172.6-171.2c4.7-4.7 4.7-12.3 .1-17z"/>
                                     </svg>
                                 </button>
